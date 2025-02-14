4acf4be2bf5eeb302d09a8e4004217c525dc9990
Marcus Ceradini <mnceradini@my.waketech.edu>
2025-02-14 12:44:02 -0500

    something changed

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-let randomNumber = Math.floor(Math.random() * 998) + 1;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 let attempts = 1;
 let guess = window.prompt('Enter a guess for the number between 1 and 999')
 guess = Number(guess)
 
 
-while (guess != randomNumber || guess != 999){
+while (guess != randomNumber){
     if (isNaN(guess)){
         guess = window.prompt('Enter a valid number')
     }
@@ -25,15 +25,3 @@ while (guess != randomNumber || guess != 999){
         break
     } 
 }
-// Prompt user for a number between 1 and 10, or type 999 to exit.
-
-// Create while loop that continues for as long as the user guesses are incorrect.
-// Loop should count the number of guesses.
-// Loop should exit if user enters 999 and not display "Too high!"
-// If the guess is too low, the user should be promped "Too low!" and be able to guess again.
-// If the guess is too high, the user should be promped 'Too high!" and be able to guess again.
-// If anything else is entered, the user should be prompted that their input is invalid and 
-// to guess again.
-
-// If the guess is correct, the user should be alerted of their win and 
-// be told the number of guesses they made.
